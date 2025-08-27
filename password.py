d0086a3b585102721fb87b0d8d6a06a4df62e689
Lakshay Gupta <lakshay8560.beaift25@chitkara.edu.in>
2025-08-27 21:23:37 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,9 @@
+username = input("Username")
+password=input("Passsword")
+if username=="admin":
+    if password =="1234":
+     print("Login Successful !")
+    else:
+     print("Incorrect password!") 
+else:
+     print("user not found")     
